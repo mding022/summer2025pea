596a677030fed77cbedb3c9a8f28183eedabdc9c
millerding <mding022@uottawa.ca>
2025-06-25 18:37:23 -0400

    change url

--- app/page.tsx
+++ app/page.tsx
@@ -90,8 +90,8 @@ interface GISResponse {
   query: string
 }
 
-// const baseUrl = "https://service.millerding.com"
-const baseUrl = "http://localhost:8000"
+const baseUrl = "https://service.millerding.com"
+// const baseUrl = "http://localhost:8000"
 
 export default function SearchDashboard() {
   const [query, setQuery] = useState("")
